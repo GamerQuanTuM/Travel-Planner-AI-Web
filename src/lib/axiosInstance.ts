d6ea1f23e0d12a04f3c9d6f7225ddb6291049ea4
GamerQuanTuM <shuvamsantra10@gmail.com>
2024-07-27 12:49:36 +0530

    changed url to render

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const prodURL = "https://travel-ai-omega.vercel.app/api/v1";
+const prodURL = "https://travel-planner-ai-web.onrender.com/api/v1";
 const devURL = "http://localhost:3000/api/v1"
 
 const axiosInstance = axios.create({
