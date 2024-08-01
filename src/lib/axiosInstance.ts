c03c304c709160069b6a7141bc04633fea2bc64d
GamerQuanTuM <shuvamsantra10@gmail.com>
2024-08-01 20:50:46 +0530

    completed

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -3,7 +3,7 @@ const prodURL = "https://travel-planner-ai-web-1.onrender.com";
 const devURL = "http://localhost:3000"
 
 const axiosInstance = axios.create({
-    baseURL: `${devURL}/api/v1`
+    baseURL: `${prodURL}/api/v1`
 })
 
 export default axiosInstance
